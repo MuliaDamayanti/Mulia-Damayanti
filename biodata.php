614d720521bbf73bb255f92620c4a2a927fcf922
MuliaDamayanti <114549178+MuliaDamayanti@users.noreply.github.com>
2022-09-28 11:34:33 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title> Tag Gambar dan Buat Tabel</title>
+</head>
+<body style=background-color:grey>
+        <h1><center> BIODATA MAHASISWA </h1>
+<table  align= center border="1" style=background-color:deepskyblue><font size=12>
+<tr>
+<th colspan="3" style=background-color:darkslateblue><center>
+<font size=5 font=calibri color=white> Data Diri </th>
+</tr><tr>
+<td> Nama </td>
+<td> Mulia Damayanti </td>
+<td rowspan="10" colspan="3"> <img src="lia.JPEG" height=500 width=400 /> </td>
+</tr>
+<tr>
+<td> Tempat Lahir </td>
+<td> Purworejo </td>
+</tr>
+<tr>
+    <td> Tanggal Masuk </td>
+<td> 22 Juni 2020 </td>
+</tr>
+<tr>
+<td> Alamat Purworejo </td>
+<td> Jln. Ngombol - Purworejo 0 km </td>
+</tr>
+<tr>
+<td> Jalur Penerimaan </td>
+<td> Reguler </td>
+</tr>
+<tr>
+<td> Tanggal Masuk </td>
+<td> 22 Juni 2020 </td>
+</tr>
+<tr>
+<td> Program Studi</td>
+<td> S1 Teknologi Informasi </td>
+</tr>
+<tr>
+<td>Dosen Penasehat</td>
+<td> Krisna Wedatama </td>
+</tr>
+<tr>
+<td> NIM</td>
+<td> 202520020 </td>
+</tr>
+<tr>
+    <td> Semester </td>
+    <td> 5 </td>
+    </tr>
+    <tr> 
+        <td> Kelas </td>
+        <td> A </td>
+    </tr>
+    <tr>
+        <td> Alamat E-mail</td>
+        <td> muliadamayantiramadhani@gmail.com </td>
+    </tr>
+    <tr>
+        <td> Perguruan Tinggi </td>
+        <td> Universitas Muhammadiyah Purworejo </td>
+    </tr>
+    <tr>
+        <td> Bidang Minat </td>
+        <td> Menari </td>
+</tr>
+<tr>
+</tr>
+</body>
+</html>
